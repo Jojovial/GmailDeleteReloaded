34e1965d75544b518af9a60715612f32f1d18550
JoJo <joehearthstone@gmail.com>
2024-02-16 15:24:38 -0500

    main api set up now for a big function

--- main.py
+++ main.py
@@ -24,3 +24,20 @@ def get_gmail_service():
             creds = flow.run_local_server(port=0)
     service = build('gmail', 'v1', credentials=creds)
     return service
+
+def list_messages(service, query=''):
+    response = service.users().messages().list(userId='me', q=query).execute()
+    messages = response.get('message', [])
+    return messages
+
+def mark_as_read(service, message_id):
+    service.users().messages().modify(
+        userId='me',
+        id=message_id,
+        body={'removeLabelIds': ['UNREAD']}
+    ).execute()
+
+def delete_message(service, message_id):
+    service.users().messages().delete(userId='me', id=message_id).execute()
+
+
